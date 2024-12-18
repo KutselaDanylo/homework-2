c1f18c02a2e05fd0b0d0e7e5479b930ce8572f1b
KutselaDanylo <160267047+KutselaDanylo@users.noreply.github.com>
2024-12-18 17:40:16 +0100

    addig lesson

---
+++ script1.js
@@ -0,0 +1,112 @@
+// const a = 1;
+// let userName ;
+// console.log(typeof userName);
+// let age = 2; 
+// console.log(typeof age);
+// let sureName = "sigma";
+// console.log( typeof sureName);
+// let massige = confirm("please confirm ");
+// let userAge = prompt("please enter your age");
+// console.log(userAge)
+// alert("please confirm your reserwation")
+// let isConfirmed = confirm(`are you shure you wont to visit this site ${age}`);
+
+
+
+// const x =10;
+//  const y =52;
+
+//  console.log(x);
+ 
+//  console.log(y)
+
+//   console.log("x > y", x > y);
+//   console.log("x > y", x < y);
+//   console.log("x > y", x <=y);
+//   console.log("x >= y", x >= y);
+//   console.log("x >= y", x >= y);
+//   console.log("x === y", x === y);
+//   console.log("x !== y", x !== y);
+
+//  console.log("x == y", x == y);
+//  console.log("x != y", x != y);
+//  let num1 = 12;
+
+//  num1 +=10;
+//  console.log(num1);
+
+// confirm("aasdf")
+
+// let userNumb = prompt("enter numer");
+// let userNumb2 = prompt("enter second number");
+// console.log(userNumb + userNumb2);
+
+// console.log(userNumb - userNumb2);
+
+// console.log(userNumb * userNumb2);
+
+// console.log(userNumb / userNumb2);
+
+// console.log(userNumb % userNumb2);
+
+
+// let userAge = prompt("please enter your age");
+// let massige = confirm(`is your age ${userAge}`);
+// console.log(typeof userAge)
+// let userName = prompt("please enter your name");
+// console.log(typeof userName)
+// let isConfirmed = confirm(`is your name ${userName}`);
+
+// const x = '10';
+// console.log(typeof x);
+// console.log(Number(x));
+// console.log(typeof Number(x));
+
+// console.text ='random';
+// console.log(Number(text));
+// console.log(typeof Number(text));
+
+// let Num1 = prompt("enter numer");
+// let Num2 = prompt("enter second number");
+// const x=Number(Num1);
+// const y=Number(Num2);
+// let massige = confirm(" do you wont to see the result ");
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+
+// .toFixed(3)
+// console.log(Number.isNaN(num));!!
+
+// console.log(Number.parseInt('5px'));
+// console.log(Number.parseInt('5.3px54'));
+// console.log(Number.parseFloat('5.3px54'));
+// console.log(Number.parseFloat('5px45'));
+
+// const age = prompt("enter your Age");
+// const height = prompt("enter your height");
+// let age1 =(Number.parseInt(age);
+// let height2 =(Number.parseInt(age))
+// console.log(age);
+// console.log(height);
+// const massige = confirm (`is your age ${age}`);
+// const massige2 = confirm (`is your height ${height}`);
+// console.log("age2 > height2", age1 > height2 );
+
+//  let x = '51';
+//  let y = Number.parseInt(x);
+//  console.log(Number.isNaN(y));
+// const num = prompt("your number");
+// let x = num;
+
+
+// const num1 = prompt("first number");
+// const num2 = prompt("second number");
+// console.log(Number.parseInt(num1));
+// console.log(Number.parseInt(num2));
+// console.log(Math.min(num1, num2));
+// console.log(Math.max(num1, num2));
+// console.log(Math.pow(num1, num2));
+// console.log(Math.pow(num2, num1));
---
+++ script2.js
@@ -0,0 +1,20 @@
+// const firstName = prompt ("please enter your first name");
+// const secondName =  prompt (" please enter your second name");
+// console.log(firstName+" "+ secondName);
+ 
+const text = prompt ("your text");
+const word = prompt ("your word");
+
+console.log(text);
+console.log(word);
+console.log(text.includes(word));
+console.log(text.indexOf(word));
+console.log(text.includes(word));
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+alert(text);
+alert(word);
+alert(text.includes(word));
+alert((text.indexOf(word)));
+alert(text.toUpperCase());
+alert(text.toLowerCase());
